7e348e6ac3b3f57767bf7e29aa4f6eff4dfe970e
YangZhenxun <2210067895@qq.com>
2022-07-20 10:30:25 +0800

    It will be done, but now isn't.

--- make-PPT-US-.pptx.py
+++ make-PPT-US-.pptx.py
@@ -1,6 +1,5 @@
 print("Note:Before make a PPT(.pptx) file,please write the info.xlsx file first.")
-from pptx import Presentation as PreS
-from pptx.util import Pt
+from pptx import Presentation
 import pandas as pd
 import time
 from tqdm import tqdm
@@ -9,29 +8,32 @@ exda = {
     'excel(Yes/None)':None,
     'pictrue(Yes/None)':None,
     'pagenum(num)':None,
+    'style(1 - 11)':None,
         }
 d_s_e_f = pd.DataFrame(exda,index=[0])
-d_s_e_f.to_excel('info.xlsx')
+d_s_e_f.to_excel('info.xlsx',sheet_name = 'read')
 class wait(object):
     def to_wait(self):
         try:
-            self.wait_time = int(input("Please input your wait time for write info.xlsx:"))
+            self.wait_time = int(input("Please input your wait time for write info.xlsx(s):"))
             for i in tqdm(range(self.wait_time)):
                 time.sleep(1)
         except:
             pass
 
-def make_PPT():
-    print()
+def make_PPT(style):
+    ppt =Presentation()
+    slide = ppt.slides.add_slide(ppt.slide_layouts[style])
+    ppt.save("aa.pptx")
 wait().to_wait()
 incase_OK = input("Are you write info.xlsx OK? (y/n):")
 if incase_OK == "y" or incase_OK  == "Y":
-    make_PPT()
+    make_PPT(style = pd.read_excel("./info.xlsx",sheet_name = "read",usecols = ['style(1 - 11)']))
 elif incase_OK == "n" or incase_OK == "N":
     wait().to_wait()
     incase_OK_1 = input("Are you write info.xlsx OK? (y/n):")
     if incase_OK_1 == "y" or incase_OK_1 == "Y":
-        make_PPT()
+        make_PPT(style = pd.read_excel("./info.xlsx",sheet_name = "read",usecols = ['style(1 - 11)']))
     else:
         pass
 else:
@@ -39,6 +41,6 @@ else:
     wait().to_wait()
     incase_OK_1 = input("Are you write info.xlsx OK? (y/n):")
     if incase_OK_1 == "y" or incase_OK_1 == "Y":
-        make_PPT()
+        make_PPT(style = pd.read_excel("./info.xlsx",sheet_name = "read",usecols = ['style(1 - 11)']))
     else:
         pass
