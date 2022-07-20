3a760f02e52e9a8316fc101a0f5d7f24112d1a1b
YangZhenxun <2210067895@qq.com>
2022-07-20 08:20:22 +0800

    It will be down, but now isn't.

--- make-PPT-US-.pptx.py
+++ make-PPT-US-.pptx.py
@@ -3,7 +3,7 @@ from pptx import Presentation as PreS
 from pptx.util import Pt
 import pandas as pd
 import time
-from alive_progress import alive_bar
+from tqdm import tqdm
 
 exda = {
     'excel(Yes/None)':None,
@@ -12,9 +12,33 @@ exda = {
         }
 d_s_e_f = pd.DataFrame(exda,index=[0])
 d_s_e_f.to_excel('info.xlsx')
-wait_time = int(input("Please input your wait time for write info.xlsx:"))
-with alive_bar(len(range(wait_time))) as bar:
-    for i in range(wait_time):
-        time.sleep(1)
-        bar()
+class wait(object):
+    def to_wait(self):
+        try:
+            self.wait_time = int(input("Please input your wait time for write info.xlsx:"))
+            for i in tqdm(range(self.wait_time)):
+                time.sleep(1)
+        except:
+            pass
+
+def make_PPT():
+    print()
+wait().to_wait()
 incase_OK = input("Are you write info.xlsx OK? (y/n):")
+if incase_OK == "y" or incase_OK  == "Y":
+    make_PPT()
+elif incase_OK == "n" or incase_OK == "N":
+    wait().to_wait()
+    incase_OK_1 = input("Are you write info.xlsx OK? (y/n):")
+    if incase_OK_1 == "y" or incase_OK_1 == "Y":
+        make_PPT()
+    else:
+        pass
+else:
+    print("Only y or n.")
+    wait().to_wait()
+    incase_OK_1 = input("Are you write info.xlsx OK? (y/n):")
+    if incase_OK_1 == "y" or incase_OK_1 == "Y":
+        make_PPT()
+    else:
+        pass
