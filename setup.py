1e6276c2085979f6523624b095ae89ff0286e376
YangZhenxun <2210067895@qq.com>
2022-07-22 10:06:03 +0800

    The new!

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {'packages': [], 'excludes': []}
 
-setup(  name = 'make-PPT-BETA.1.py',
-        version = 'v0.0.1-beta.1',
+setup(  name = 'make-PPT-b1.py',
+        version = 'v0.0.1-b1',
         description = 'It can make a PPT for you',
         options = {'build_exe': build_exe_options},
-        executables = [Executable('./.beta-1.py')])
+        executables = [Executable('./b1.py')])
