787ad6c654e6ae1dd9ae6ea4d4f112183a19111d
杨震勋 <2210067895@qq.com>
2024-08-19 06:42:26 +0800

    Update lexer.py

--- PySide/lexer.py
+++ PySide/lexer.py
@@ -1,39 +1,10 @@
-import ply.lex as lex
+import importlib
+from pathlib import Path
 
-tokens = (
-    'ID',
-    "PLUS",
-    "MINUS",
-    "TIMES",
-    "DIVIDE",
-    "LPAREN",
-    "PPAREN"
-)
-
-reverseds = {
-    "if" : "IF"
-}
-
-t_PLUS = r"\+"
-t_MINUS = r"-"
-t_TIMES = r"/"
-t_DIVIDE = r"\*"
-t_LPAREN = r"\("
-t_RPAREN = r"\)"
-
-def t_ID(t):
-    r"[a-zA-Z_0-9]*"
-    t.type = reverseds.get(t.value, "ID")
-    return t
-
-def t_newline(t):
-    r"\n+"
-    t.lexer.lineno += len(t.value)
-
-t_ignore = " \t"
-
-def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
-    t.lexer.skip(1)
-
-lexer = lex.lex()
+def main(files: list[Path]):
+    i: int = 1
+    for file in files:
+        p: Path = Path("./tmp"+str(i)+".py")
+        p.write_text("from tools import *\n"+file.read_text())
+        importlib.import_module(p.stem)
+        p.unlink()
